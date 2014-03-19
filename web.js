9000ed7080e473ccf511a40877c3a931768710ec
dooztron <xcxd.chen@gmail.com>
2014-03-18 23:24:13 -0400

    new shit

--- web.js
+++ web.js
@@ -6,6 +6,12 @@ var app = express(),
 
 app.use(logfmt.requestLogger());
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 
 app.configure( function() { 
   app.use('/js', express.static(__dirname + '/js')); 
